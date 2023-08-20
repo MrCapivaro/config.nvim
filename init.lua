4fc2a5b34183e07791947a76b37095d79bec7d01
MrCapivaro <guilhermepachecobatista@gmail.com>
2023-08-20 20:00:38 -0300

    update

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
-require("core.keymaps")
-require("core.options")
-require("extras.lazy")
+if vim.g.vscode then
+  require("core.keymaps")
+  require("core.options")
+else
+  require("core.keymaps")
+  require("core.options")
+  require("extras.lazy")
+end
